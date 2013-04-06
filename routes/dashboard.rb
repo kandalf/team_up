cb3ae64e5692c41432325c1fde4121aae81e74b6
Leonardo Mateo <leonardomateo@gmail.com>
2013-04-06 12:08:06 -0300

    Redirects to dashboard from root

--- routes/dashboard.rb
+++ routes/dashboard.rb
@@ -2,13 +2,13 @@ module TeamUp
   class Dashboard < Cuba
     define do
       on root do
-        res.write render("./views/layouts/application.haml") {
-          render("views/dashboard/dashboard.haml")
-        }
+        res.redirect "/dashboard"
       end
 
       on "dashboard" do
-        res.redirect "/"
+        res.write render("./views/layouts/application.haml") {
+          render("views/dashboard/dashboard.haml")
+        }
       end
     end
   end
