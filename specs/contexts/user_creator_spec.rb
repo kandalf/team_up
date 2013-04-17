d80a2b286fdfb92300f8d659b141f3b3be746aab
Leonardo Mateo <leonardomateo@gmail.com>
2013-04-17 08:43:28 -0300

    Adds contexts specs

---
+++ specs/contexts/user_creator_spec.rb
@@ -0,0 +1,35 @@
+require File.expand_path(File.dirname(__FILE__)) + '/../test_helper'
+
+describe SessionManager do
+  def setup
+    @attributes = {
+      :full_name => "Ed Vedder",
+      :github_user => "pearljam",
+      :email => "eddie@example.com",
+      :gravatar_url => "http://somepic.com",
+      :password => "testpass",
+    }
+
+    @auth_hash = {
+      "uid" => "1111",
+      "info" => {
+        "name" => "Jeff Ament",
+        "nickname" => "jeff",
+        "email" => "jeff@example.com",
+        "image" => "http://example.com/pic.jpg",
+      },
+      "extra" => {}
+    }
+  end
+
+  it "should signup from the auth_hash" do
+    user = User.with(:email, @auth_hash["info"]["email"])
+    user.must_be_nil
+
+    user = SessionManager.new(@auth_hash, Cuba.app).execute
+    user.wont_be :nil?
+    
+    user = User.with(:email => @auth_hash["info"]["email"])
+    user.wont_be_nil
+  end
+end
