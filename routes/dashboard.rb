87e5c33fd41693c3b1d7dd3767960d551da43e27
Leonardo Mateo <leonardomateo@gmail.com>
2013-06-02 19:38:28 -0300

    Adds standups index.
    Extracts standup entry to a partial

--- routes/dashboard.rb
+++ routes/dashboard.rb
@@ -2,7 +2,7 @@ module TeamUp
   class Dashboard < Cuba
     define do
       on get do
-        @standups = ::Standup.all.sort_by(:date, :order => "ALPHA DESC")
+        @standups = ::Standup.for(Date.today).sort_by(:date, :order => "ALPHA DESC")
         @my_standup = current_user.today_standup
         res.write render("./views/layouts/application.haml") {
           render("views/dashboard/dashboard.haml")
