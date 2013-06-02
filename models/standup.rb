d3d74a199a82b31483f2fb29a1813e6007e74c2e
Leonardo Mateo <leonardomateo@gmail.com>
2013-06-02 19:36:28 -0300

    Fixes style for dashboard

--- models/standup.rb
+++ models/standup.rb
@@ -15,4 +15,16 @@ class Standup < Ohm::Model
     assert_present :previous
     assert_present :next
   end
+
+  def editable?
+    !!(self.date == Date.today.to_s)
+  end
+
+  def self.for(date)
+    find(:date => date.to_s)
+  end
+
+  def self.last_ones
+    all.except(:date => Date.today.to_s).sort_by(:date, :order => "ALPHA DESC")
+  end
 end
--- routes/standups.rb
+++ routes/standups.rb
@@ -10,6 +10,30 @@ module TeamUp
       end
       res.redirect("/dashboard")
     end
+
+    on get do
+      on root do
+        today_standups = ::Standup.for(Date.today)
+        previous = ::Standup.last_ones
+
+        res.write render("./views/layouts/application.haml") {
+          render("views/standups/index.haml", {:for_today => today_standups, :previous => previous})
+        }
+      end
+
+      on ":id/edit" do |id|
+        standup = current_user.standups[id]
+
+        if standup
+          res.write render("./views/layouts/application.haml") {
+            render("views/standups/edit.haml", {:standup => standup})
+          }
+        else
+          res.status = 404
+          res.write("Not Found")
+        end
+      end
+    end
   end
  end
 end
