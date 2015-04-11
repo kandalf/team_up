5c1d9d25e00d56482b5f71f089a267e2ea64feaa
Leonardo Mateo <leonardomateo@gmail.com>
2015-04-11 17:33:46 -0300

    Removes TeamUp namespace for standups routes

--- routes/sessions.rb
+++ routes/sessions.rb
@@ -1,24 +1,22 @@
 require 'pp'
 
-module TeamUp
-  class Session < Cuba
-    define do
-      on ":provider/callback" do
-        user = SessionManager.new(@env['omniauth.auth'], self).execute
+class SessionRoutes < Cuba
+  define do
+    on ":provider/callback" do
+      user = SessionManager.new(@env['omniauth.auth'], self).execute
 
-        if user
-          res.redirect "/dashboard"
-        else
-          logout(User) if user
-          session['flash.error'] = 'Unauthorized User'
-          res.redirect '/'
-        end
+      if user
+        res.redirect "/dashboard"
+      else
+        logout(User) if user
+        session['flash.error'] = 'Unauthorized User'
+        res.redirect '/'
       end
+    end
 
-      on "failure" do
-        res.headers["Content-type"] = "text/plain"
-        res.write("Unauthorized")
-      end
+    on "failure" do
+      res.headers["Content-type"] = "text/plain"
+      res.write("Unauthorized")
     end
   end
 end
