3b6dec4d81335a8eb467214a2b960ef8b96fd00a
Leonardo Mateo <leonardomateo@gmail.com>
2015-04-11 14:03:04 -0300

    Fixes dashboard view

--- routes/dashboard.rb
+++ routes/dashboard.rb
@@ -1,12 +1,12 @@
 module TeamUp
   class Dashboard < Cuba
-    #Cuba.settings[:render][:layout] = "layouts/application"
+    settings[:render][:layout] = "layouts/application"
 
     define do
       on get do
-        @standups = ::Standup.for(Date.today).order_by(Sequel.desc(:date))
-        @my_standup = current_user.today_standup
-        res.write render("dashboard/dashboard", {}, "layouts/application")
+        standups = ::Standup.for(Date.today).order_by(Sequel.desc(:date))
+        my_standup = current_user.today_standup
+        res.write view("dashboard/dashboard", {:standups => standups, :my_standup => my_standup})
       end
     end
   end
