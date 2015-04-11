b0f3c7d83fab9395a070bb07b7d835a0d9dab5e9
Leonardo Mateo <leonardomateo@gmail.com>
2015-04-11 17:33:03 -0300

    Removes TeamUp namespace for dashboard routes

--- routes/dashboard.rb
+++ routes/dashboard.rb
@@ -1,13 +1,11 @@
-module TeamUp
-  class Dashboard < Cuba
-    settings[:render][:layout] = "layouts/application"
+class DashboardRoutes < Cuba
+  settings[:render][:layout] = "layouts/application"
 
-    define do
-      on get do
-        standups = ::Standup.for(Date.today).order_by(Sequel.desc(:date))
-        my_standup = current_user.today_standup
-        res.write view("dashboard/dashboard", {:standups => standups, :my_standup => my_standup})
-      end
+  define do
+    on get do
+      standups = Standup.for(Date.today).order_by(Sequel.desc(:date))
+      my_standup = current_user.today_standup
+      res.write view("dashboard/dashboard", {:standups => standups, :my_standup => my_standup})
     end
   end
 end
